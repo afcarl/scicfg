a11834172b666f7643eef9ecccecac397405e35e
Fabien Benureau <fabien.benureau+git@gmail.com>
2015-11-05 17:13:49 +0100

    test fixes for python 3

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, division
 import unittest
+import collections
+import types
 
 import env
 import scicfg
@@ -18,6 +20,12 @@ class TestIterators(unittest.TestCase):
         self.assertEqual({1, 'blue'}, set(tc._values()))
         self.assertEqual({('a.b', 1), ('color', 'blue')}, set(tc._items()))
 
+    @classmethod
+    def listit(cls, a):
+        #print(a.__class__)
+        if isinstance(a, collections.Iterable) and not isinstance(a, str):
+            return [cls.listit(e) for e in a]
+        return a
 
     def test_walk(self):
         cfg = scicfg.SciConfig()
@@ -25,7 +33,7 @@ class TestIterators(unittest.TestCase):
         cfg['a.b.d'] = 2
         cfg['a.e']   = 3
 
-        self.assertEqual(list(cfg._walk()), [('', ['a'], []), ('a', ['b'], ['e']), ('a.b', [], ['c', 'd'])])
+        self.assertEqual(self.listit(cfg._walk()), [['', ['a'], []], ['a', ['b'], ['e']], ['a.b', [], ['d', 'c']]])
 
 if __name__ == '__main__':
     unittest.main()
