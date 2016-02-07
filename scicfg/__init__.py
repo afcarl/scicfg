63c4c5c794114ed1606124806b55aa6a56ecb689
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-02-07 20:25:18 +0100

    __url__ attribute

--- scicfg/__init__.py
+++ scicfg/__init__.py
@@ -1,9 +1,13 @@
-from .configs import SciConfig
-from .types import string
-
-
+# versioneer
 from ._version import get_versions
 __version__ = get_versions()["version"]
 __commit__ = get_versions()["full-revisionid"]
 __dirty__ = get_versions()["dirty"]
 del get_versions
+
+__url__ = 'https://github.com/humm/scicfg'
+
+
+# intra-package imports
+from .configs import SciConfig
+from .types import string
